463cbe1b88c75ac778dca06b344a2c1e7a79efa6
seongmin kim <k2417000@naver.com>
2024-11-04 17:43:19 +0900

    [feat] Font, 탭바 이미지 추가, DMView 추가

---
+++ Gathering/Design/Font+.swift
@@ -0,0 +1,16 @@
+//
+//  Font+.swift
+//  Gathering
+//
+//  Created by 김성민 on 11/4/24.
+//
+
+import SwiftUI
+
+extension Font {
+    static let title1 = Font.system(size: 22, weight: .bold)
+    static let title2 = Font.system(size: 14, weight: .bold)
+    static let bodyBold = Font.system(size: 13, weight: .bold)
+    static let body = Font.system(size: 13, weight: .regular)
+    static let caption = Font.system(size: 12, weight: .bold)
+}
