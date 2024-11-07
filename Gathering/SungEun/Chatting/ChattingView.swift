602a8bbcb33fb80fbfb98b2904157eaff163383b
YeoSungeun <dutjddms@naver.com>
2024-11-08 00:13:58 +0900

    [Chore] View extension 수정

--- Gathering/SungEun/Chatting/ChattingView.swift
+++ Gathering/SungEun/Chatting/ChattingView.swift
@@ -301,6 +301,16 @@ struct DynamicHeightTextField: View {
     }
 }
 
+extension View {
+    // 키보드 숨기기 메서드
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder),
+                                        to: nil, 
+                                        from: nil,
+                                        for: nil)
+    }
+}
+
 #Preview {
     ChattingView()
 }
