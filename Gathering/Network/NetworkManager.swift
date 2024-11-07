8dd779cd1f716ca45b8db866c40b714ab2333b7c
seongmin kim <k2417000@naver.com>
2024-11-07 21:19:17 +0900

    [Feat] 네트워크 매니저 파일 추가

---
+++ Gathering/Network/NetworkManager.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkManager.swift
+//  Gathering
+//
+//  Created by 김성민 on 11/7/24.
+//
+
+import Foundation
+
+final class NetworkManager {
+    static let shared = NetworkManager()
+    private init() {}
+}
