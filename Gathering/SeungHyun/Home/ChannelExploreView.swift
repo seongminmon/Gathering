4dac4bb03eb7b0430dbf93d2d89261e0fba4d85a
seunghyun Lee <yoruck2@gmail.com>
2024-11-08 21:22:02 +0900

    [Feat] ChannelExploreView 추가

--- Gathering/SeungHyun/Home/ChannelExploreView.swift
+++ Gathering/SeungHyun/Home/ChannelExploreView.swift
@@ -5,4 +5,34 @@
 //  Created by dopamint on 11/8/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct ChannelExploreView: View {
+    var body: some View {
+        let channels = Dummy.channels
+        SheetHeaderView(title: "채널 탐색")
+        
+        ScrollView {
+            VStack(alignment: .leading, spacing: 20) {
+                ForEach(channels, id: \.id) { channel in
+                    HStack(spacing: 12) {
+                        ProfileImageView(imageName: "hashTagthick",
+                                         size: 18)
+                        
+                        Button(action: {
+                            // TODO: -
+                        }) {
+                            Text(channel.name)
+                                .font(.title2)
+                                .tint(.black)
+                        }
+                        Spacer()
+                    }
+                    .padding(.horizontal, 16)
+                }
+            }
+            .padding(.vertical, 20)
+        }
+    }
+}
+
