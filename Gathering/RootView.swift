ef6b735c9aaf32c312c1e70f2600a7f9daa88951
seunghyun Lee <yoruck2@gmail.com>
2024-11-08 21:20:08 +0900

    [Feat] InviteMemnerView 추가

--- Gathering/RootView.swift
+++ Gathering/RootView.swift
@@ -27,9 +27,9 @@ struct RootView: View {
     private func tabView() -> some View {
         TabView(selection: $tabInfo) {
             // MARK: - 홈
-            GatheringNavigationStack {
+            GatheringNavigationStack(content: {
                 HomeView()
-            }
+            }, gatheringImage: "bird2", profileImage: "bird3")
             .tabItem {
                 Image(tabInfo == .home ? .homeActive : .homeInactive)
                 Text(TabInfo.home.rawValue)
