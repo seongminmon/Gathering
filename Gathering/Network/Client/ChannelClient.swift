04c76e1d6e2d6b19f141ad40d69694fea68fa2d8
seongmin kim <k2417000@naver.com>
2024-11-11 22:20:22 +0900

    [Feat] API 공통 Error 구현

---
+++ Gathering/Network/Client/ChannelClient.swift
@@ -0,0 +1,8 @@
+//
+//  ChannelClient.swift
+//  Gathering
+//
+//  Created by 김성민 on 11/11/24.
+//
+
+import Foundation
---
+++ Gathering/Network/Router/Channel/ResponseModel/UnreadCountResponse.swift
@@ -0,0 +1,8 @@
+//
+//  UnreadCountResponse.swift
+//  Gathering
+//
+//  Created by 김성민 on 11/11/24.
+//
+
+import Foundation
