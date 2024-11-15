7f3b0581469549727a1683ce490e7ea961946749
seongmin kim <k2417000@naver.com>
2024-11-15 16:38:34 +0900

    [Remove] 네트워크 테스트 코드 삭제

--- Gathering/GatheringApp.swift
+++ Gathering/GatheringApp.swift
@@ -36,29 +36,6 @@ struct GatheringApp: App {
                 OnboardingView(
                     store: Store(initialState: OnboardingFeature.State()) { OnboardingFeature() }
                 )
-                .task {
-                    UserDefaultsManager.accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiOTczZDYyZWMtMTc3Ni00NDZmLTkwZWEtZjM1ZDE4OWJiN2IzIiwibmlja25hbWUiOiJrc20xIiwiaWF0IjoxNzMxNjU0OTU0LCJleHAiOjE3MzE2NTUyNTQsImlzcyI6InNscCJ9.rfucZPQ_mqMg5VxhZ7LkXO9N3glVG07s_yTXD1G-Pbk"
-                    
-                    UserDefaultsManager.refreshToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiOTczZDYyZWMtMTc3Ni00NDZmLTkwZWEtZjM1ZDE4OWJiN2IzIiwiaWF0IjoxNzMxNjU0OTU0LCJleHAiOjE3MzE2NTg1NTQsImlzcyI6InNscCJ9.fZuxhT2z-L9MTnTZ_8BULK-Fsp1i6i7Qg_2Zu2_LjhY"
-  
-                    // 토큰 갱신 테스트
-//                    do {
-//                        let result: Token = try await NetworkManager.shared.request(
-//                            api: AuthRouter.refreshToken(refreshToken: UserDefaultsManager.refreshToken)
-//                        )
-//                        print(result)
-//                    } catch {
-//                        print("error")
-//                    }
-                    
-                    // MARK: - 네트워크 테스트
-                    do {
-                        let result: [WorkspaceResponse] = try await NetworkManager.shared.request(api: WorkspaceRouter.fetchMyWorkspaceList)
-                        print(result)
-                    } catch {
-                        print(error)
-                    }
-                }
             }
         }
     }
